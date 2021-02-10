c01ac916c467b3bd24d9dac266555c3864228579
venkatesh179 <venkateshdyarangula@gmail.com>
2021-02-10 11:07:20 +0530

    Inputs are Taken

--- arithmaticComputaionAndSorting.sh
+++ arithmaticComputaionAndSorting.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to ArithmaticComputaion"
+takeInputs (){
+read -p "Enter a Value:" a
+read -p "Enter b Value:" b
+read -p "Enter c Value:" c
+echo "a b c Values are:" $a $b $c
+}
+takeInputs
+
